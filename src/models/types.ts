24b91ffc81fc6badf1c586ffa718a7858bcf1f50
Khairo Khatib <khairo.khatib@gmail.com>
2022-09-22 13:51:14 -0400

    created state store for the cluster data

--- src/models/types.ts
+++ src/models/types.ts
@@ -1,1 +1,9 @@
 export type EndpointTypes = 'mainnet' | 'devnet' | 'localnet'
+
+export interface ClusterStats {
+  absoluteSlot: number;
+  blockHeight: number | undefined;
+  blockTime: number;
+  currentEpoch: string;
+  epochProgress: string;
+};
